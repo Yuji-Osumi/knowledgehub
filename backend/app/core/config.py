4e98fe84d3b774796acf13cf3ae0badd692538ea
Yuji0045 <osyj5400@gmail.com>
2026-01-07 20:12:44 +0900

    feat: .env周りの微調整

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -24,14 +24,10 @@ class AppSettings(BaseSettings):
 
     # --- Database ---
     DATABASE_URL: str
-    db_host: str
-    db_port: int
-    db_name: str
-    db_user: str
-    db_password: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
+        extra="ignore",
         env_file_encoding="utf-8",
     )
 
