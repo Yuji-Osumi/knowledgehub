f070659a0856b508de85a23a3eb2f20975f3804f
Yuji0045 <osyj5400@gmail.com>
2026-01-18 17:33:55 +0900

    feat：記事作成APIを実装

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.articles import router as articles_router
 from app.api.health import router as health_router
 
 api_router = APIRouter()
@@ -9,4 +10,11 @@ api_router.include_router(
     health_router,
     prefix="",
     tags=["Health"],
+)
+
+# 記事管理
+api_router.include_router(
+    articles_router,
+    prefix="/articles",
+    tags=["articles"],
 )
